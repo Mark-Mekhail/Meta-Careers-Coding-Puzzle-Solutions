9573203b2819692a37f5617e3a4e43544880e59a
Mark-Mekhail <markm2hs@gmail.com>
2024-10-14 21:37:13 -0700

    Fix boss fight optimization bug

--- Level 3/Boss Fight/python/Boss_Fight.py
+++ Level 3/Boss Fight/python/Boss_Fight.py
@@ -6,16 +6,18 @@ def getMaxDamageDealt(N: int, H: List[int], D: List[int], B: int) -> float:
   damageIsSame = False
   while not damageIsSame:
     damageIsSame = True
-    for i in range(N):
-      if i != curPair[0]:
-        damage = calculateDamage(H, D, curPair[0], i)
-        if damage > curDamage:
-          damageIsSame = False
-          curDamage = damage
-          curPair[1] = i
-    curPair.reverse()
+    for _ in range(2):
+      for i in range(N):
+        if i != curPair[0]:
+          damage = calculateDamage(H, D, curPair[0], i)
+          if damage > curDamage:
+            damageIsSame = False
+            curDamage = damage
+            curPair[1] = i
+      curPair.reverse()
       
   return curDamage / B
+  
   
 def calculateDamage(H, D, warrior1, warrior2):
   return H[warrior1] * D[warrior1] + H[warrior2] * D[warrior2] + max(H[warrior1] * D[warrior2], H[warrior2] * D[warrior1])
