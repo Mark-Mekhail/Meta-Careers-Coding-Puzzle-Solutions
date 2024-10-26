71871edeccf90994b09d799790ff6575f736dee6
Mark-Mekhail <markm2hs@gmail.com>
2024-10-25 17:55:20 -0700

    Add Python solution for Cafeteria

---
+++ Level 1/Cafeteria/python/Cafeteria.py
@@ -0,0 +1,27 @@
+from typing import List
+
+def getMaxAdditionalDinersCount(N: int, K: int, M: int, S: List[int]) -> int:
+    S.sort()
+    print(S)
+
+    cur_start = 1
+    additional_diners_count = 0
+
+    for i in range(M):
+        cur_seat = S[i]
+
+        free_seats = cur_seat - K - cur_start
+        if free_seats > 0:
+            additional_diners_count += free_seats // (K + 1)
+            if free_seats % (K + 1) > 0:
+                additional_diners_count += 1
+
+        cur_start = cur_seat + K + 1
+
+    if N + 1 > cur_start:
+        free_seats = N + 1 - cur_start
+        additional_diners_count += free_seats // (K + 1)
+        if free_seats % (K + 1) > 0:
+            additional_diners_count += 1
+
+    return additional_diners_count
