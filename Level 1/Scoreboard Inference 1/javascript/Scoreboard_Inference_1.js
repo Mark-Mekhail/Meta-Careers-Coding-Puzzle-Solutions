abbd646d797e636640f82600e54ab102c9ec38a2
Mark-Mekhail <markm2hs@gmail.com>
2024-10-25 18:01:01 -0700

    Fix more js formatting

--- Level 1/Scoreboard Inference 1/javascript/Scoreboard_Inference_1.js
+++ Level 1/Scoreboard Inference 1/javascript/Scoreboard_Inference_1.js
@@ -7,12 +7,12 @@ function getMinProblemCount(N, S) {
     max = -1
     containsOdd = false
     for (score of S) {
-      if (score > max) {
-        max = score
-      }
-      
-      containsOdd = containsOdd || (score % 2 == 1)
+        if (score > max) {
+            max = score
+        }
+        
+        containsOdd = containsOdd || (score % 2 == 1)
     }
     
     return Math.floor(max / 2) + (containsOdd ? 1 : 0)
-  }
+}
