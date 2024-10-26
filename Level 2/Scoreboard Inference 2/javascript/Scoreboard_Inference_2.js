76886502b1406f62fbc5117adf3bc2fa46a0695a
Mark-Mekhail <markm2hs@gmail.com>
2024-10-25 18:34:16 -0700

    Fix js formatting

--- Level 2/Scoreboard Inference 2/javascript/Scoreboard_Inference_2.js
+++ Level 2/Scoreboard Inference 2/javascript/Scoreboard_Inference_2.js
@@ -11,59 +11,58 @@ function getMinProblemCount(N, S) {
     let hasTwoRemainder = false
     
     for (let i = 0; i < N; i++) {
-      const score = S[i]
-      
-      if (score > max) {
-        secondMax = max
-        max = score
-      }
-      else if (score > secondMax) {
-        secondMax = score
-      }
-      
-      
-      hasOne = hasOne || score == 1
-      hasOneRemainder = hasOneRemainder || (score > 1 && (score % 3 == 1))
-      hasTwoRemainder = hasTwoRemainder || (score % 3 == 2)
+        const score = S[i]
+        
+        if (score > max) {
+            secondMax = max
+            max = score
+        }
+        else if (score > secondMax) {
+            secondMax = score
+        }
+        
+        
+        hasOne = hasOne || score == 1
+        hasOneRemainder = hasOneRemainder || (score > 1 && (score % 3 == 1))
+        hasTwoRemainder = hasTwoRemainder || (score % 3 == 2)
     }
     
     let ones = 0
     let twos = 0
     if (hasTwoRemainder) {
-      if (hasOne) {
-        ones = 1
-        twos = 1
-      }
-      else if (hasOneRemainder) {
-        if (secondMax == max - 1 && max % 3 == 1) {
-          twos = 1
+        if (hasOne) {
           ones = 1
+          twos = 1
+        }
+        else if (hasOneRemainder) {
+            if (secondMax == max - 1 && max % 3 == 1) {
+                twos = 1
+                ones = 1
+            }
+            else {
+                twos = 2
+            }
         }
         else {
-          twos = 2
+            twos = 1
         }
-        
-      }
-      else {
-        twos = 1
-      }
     }
     else if (hasOneRemainder || hasOne) {
-      ones = 1
+        ones = 1
     }
     
     let threes = Math.floor(max / 3)
     let remainingSum = twos * 2 + ones;
     if (max % 3 == 0 && remainingSum == 3) {
-      threes--;
+        threes--;
     }
     else if (max % 3 == 1 && remainingSum == 4) {
-      threes--;
+        threes--;
     }
     
     if (twos + ones > 2 || (max % 3 == 2 && twos == 0)) {
-      throw Error("ow")
+        throw Error("ow")
     }
     
     return threes + twos + ones;
-  }
+}
