86f33c3d9ba09b070a6f4f7b694e82f38d49f983
Mark-Mekhail <markm2hs@gmail.com>
2024-10-25 17:58:50 -0700

    Fix js formatting

--- Level 1/Rotary Lock 1/javascript/Rotary_Lock_1.js
+++ Level 1/Rotary Lock 1/javascript/Rotary_Lock_1.js
@@ -8,10 +8,10 @@ function getMinCodeEntryTime(N, M, C) {
     time = 0
     prev = 1
     for (i = 0; i < M; i++) {
-      digit = C[i]
-      time += Math.min(Math.abs(digit - prev), N + Math.min(prev, digit) - Math.max(prev, digit))
-      prev = digit
+        digit = C[i]
+        time += Math.min(Math.abs(digit - prev), N + Math.min(prev, digit) - Math.max(prev, digit))
+        prev = digit
     }
     
     return time
-  }
+}
