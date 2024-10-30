d66461d8b6ae163de662050180873484ea598d37
Mark Mekhail <mark.mekhail@intel.com>
2024-10-30 11:13:39 -0700

    Minor Hops optimization

--- Level 2/Hops/python/Hops.py
+++ Level 2/Hops/python/Hops.py
@@ -6,7 +6,7 @@ def getSecondsRequired(N: int, F: int, P: List[int]) -> int:
     
     first_frog_index = P[0]
     seconds = 0
-    for i in range(F):
+    for i in range(1, F):
         if P[i] > first_frog_index + i:
             seconds += P[i] - first_frog_index
             first_frog_index = P[i]
