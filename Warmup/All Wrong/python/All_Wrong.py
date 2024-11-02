0333689bb9f870866ca7864a33953f366e30bdde
Mark-Mekhail <markm2hs@gmail.com>
2024-11-02 13:25:05 -0700

    Improve warmup Python solution code

--- Warmup/All Wrong/python/All_Wrong.py
+++ Warmup/All Wrong/python/All_Wrong.py
@@ -1,10 +1,10 @@
 def getWrongAnswers(N: int, C: str) -> str:
-  wrongAnswers = []
-  for answer in C:
-    if answer == 'A':
-      wrongAnswers.append('B')
-    else:
-      wrongAnswers.append('A')
-      
-  
-  return ''.join(wrongAnswers)
+    wrongAnswers = []
+    for answer in C:
+        # If the correct answer is 'A', append the wrong answer 'B', and vice versa
+        if answer == 'A':
+            wrongAnswers.append('B')
+        else:
+            wrongAnswers.append('A')
+        
+    return ''.join(wrongAnswers)
--- Warmup/Battleship/python/Battleship.py
+++ Warmup/Battleship/python/Battleship.py
@@ -1,12 +1,13 @@
 from typing import List
 
 def getHitProbability(R: int, C: int, G: List[List[int]]) -> float:
-  numCells = R * C
-  numShipCells = 0
-  
-  for row in G:
-    for cell in row:
-      if cell == 1:
-        numShipCells += 1
-  
-  return numShipCells / numCells
+    numCells = R * C
+
+    # Count the number of cells that contain a ship
+    numShipCells = 0
+    for row in G:
+        for cellVal in row:
+            if cellVal == 1:
+                numShipCells += 1
+    
+    return numShipCells / numCells
